75ddfa58336203bc502721ba97979112ee3baca5
Milena Barbosa <milenna_231@hotmail.com>
2024-05-21 18:34:59 -0300

    first commit

---
+++ Aula HTML/Aula 6/first.js
@@ -0,0 +1,45 @@
+console.log('Olá mundo');
+
+const nomeDoPais = 'Brasil';
+let pontosTurísticos = 'Cristo redentor';
+pontosTurísticos = 'MASP'
+const capitalDoPais = 'Brasília';
+const qtdEstados =  26;
+const LATAM = true
+
+console.log(LATAM)
+
+/*Crie as seguintes constantes e em seguida use a criatividade para concatená-las, formando uma frase: */
+
+const primeiroNome = "Milena";
+const ultimoNome = 'Divino';
+const anoNascimento = 97; 
+const corPreferida = "roxo";
+let hobby = "pilates"
+let sentimento = "feliz"
+
+console.log("eu me chamo "+ primeiroNome + ", minha cor favorita é " + corPreferida);
+
+const soma = 13+2
+const subtracao = 10-9
+const multi = 2 * 2
+
+console.log(soma + " é diferente de "+ subtracao);
+
+console.log(multi)
+
+/*   C=pi*2*raio   */
+const pi = Math.PI
+const raio = 15.75
+const pi2 = pi*2
+const circ = pi2*raio
+console.log("A circunferência é igual a " + circ.toFixed(2))
+
+
+
+const hora = 10;
+if (hora<12) {
+    console.log('Bom dia!');
+} else {
+    console.log('Boa tarde!');
+}
